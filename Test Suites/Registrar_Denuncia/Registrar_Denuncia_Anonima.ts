b612c826de8cf9fda3845612d2ffe1fe157e8284
Melinna Rojas Camargo <melin1504@gmail.com>
2019-03-22 10:06:00 -0500

    TestData

--- Test Suites/Registrar_Denuncia/Registrar_Denuncia_Anonima.ts
+++ Test Suites/Registrar_Denuncia/Registrar_Denuncia_Anonima.ts
@@ -10,4 +10,19 @@
    <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <testSuiteGuid>d19d7fd1-0a3b-40e9-b469-17176522c5a3</testSuiteGuid>
+   <testCaseLink>
+      <guid>c2937bd0-4891-4658-8755-0afae52fee36</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Formulario_RegistrarDenuncia_Anonima</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>3476c74f-da7b-4501-95fc-40b173d50070</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/SQA12-Registrar Denuncia Anonima</testDataId>
+      </testDataLink>
+   </testCaseLink>
 </TestSuiteEntity>
